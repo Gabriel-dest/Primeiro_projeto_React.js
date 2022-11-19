60b548ba217dbc37897f3559a741903e09bd8fee
Gabriel-dest <113642864+Gabriel-dest@users.noreply.github.com>
2022-11-19 17:36:52 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+import Navbar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskList/TaskList";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
+  const updateTask = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, title, state };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((exitingTasks) => {
+      return exitingTasks.filter((task) => task.id !== id);
+    });
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <TaskList
+          title="Pending"
+          onAddTask={addTask}
+          taskState="Pending"
+          tasks={tasks.filter((t) => t.state === "Pending")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Making"
+          onAddTask={addTask}
+          taskState="Making"
+          tasks={tasks.filter((t) => t.state === "Making")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+        <TaskList
+          title="Complete"
+          onAddTask={addTask}
+          taskState="Complete"
+          tasks={tasks.filter((t) => t.state === "Complete")}
+          onTaskUpdate={updateTask}
+          onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+}
